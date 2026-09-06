3b993408a5c2e71455ffe97f85869f817b320dc3
damaipamungkas <139616767+damaipamungkas@users.noreply.github.com>
2026-09-06 07:54:32 +0700

    Change route for appendix link in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -47,7 +47,7 @@
     </ul>
 
     <!-- Perbaikan: Mengubah route('appendix') menjadi route('appendix.index') -->
-    <a href="{{ route('appendix') }}" class="sidebar-appendix-box {{ request()->routeIs('appendix') ? 'active' : '' }}">
+    <a href="{{ route('appendix1') }}" class="sidebar-appendix-box {{ request()->routeIs('appendix1') ? 'active' : '' }}">
         <i class="fa-solid fa-book"></i>
         <span>Appendix</span>
     </a>
